adc5d24aadca0edaaeddfa7b05b9e40de66392f6
Taylor Smith <tsmith@tsmithcreative.com>
2022-09-06 23:40:55 -0500

    Set up R2 and KV bindings and test serving a sample image from R2

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "itty-router";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  METADATA: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  STORAGE: R2Bucket;
 }
 
 const router = Router();
@@ -19,15 +19,31 @@ const basic200 = () => new Response('Paperframe backend is running');
 router.get('/api', basic200);
 
 router.get('/api/now', (request, env, context) => {
-	return new Response('Current image');
+  return new Response('See sample', {
+    status: 302,
+    headers: {
+      'Location': '/api/image/sample',
+    },
+  })
 });
 
-router.get('/api/image/:id', (request, env, context) => {
-	return new Response(`Returning image ${request.params?.id}`);
+router.get('/api/image/:id', async (request, env, context) => {
+  if (request.params?.id == 'sample') {
+    // @TODO: the Env interface typings aren't getting picked up here...
+    const sample = await env.STORAGE.get('sample.jpg');
+
+    if (sample?.body) {
+      return new Response(sample.body, {
+        headers: {
+          'content-type': 'image/jpeg',
+        },
+      });
+    }
+  }
 });
 
 router.all('*', basic404);
 
 export default {
-	fetch: router.handle,
+  fetch: router.handle,
 };
